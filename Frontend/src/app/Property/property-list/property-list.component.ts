bdceadf804c71a79c3a0f17731235f9f127790c3
Praveenkumar <praveenmca@outlook.com>
2021-03-12 01:58:42 +0530

    List of Card

--- Frontend/src/app/Property/property-list/property-list.component.ts
+++ Frontend/src/app/Property/property-list/property-list.component.ts
@@ -6,7 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-
+  Properties:Array<any>=[
+    {
+    "Id":1,
+    "Type":"House",
+    "Price":12000,
+    "Name":"Birla House"
+    },
+    {
+    "Id":2,
+    "Type":"House",
+    "Price":20000,
+    "Name":"Praveen House"
+    },
+    {
+    "Id":3,
+    "Type":"House",
+    "Price":12000,
+    "Name":"Janu House"
+    },
+    {
+    "Id":4,
+    "Type":"House",
+    "Price":12000,
+    "Name":"Appa House"
+    },
+    {
+    "Id":5,
+    "Type":"House",
+    "Price":12000,
+    "Name":"Birla House"
+    },
+    {
+    "Id":6,
+    "Type":"House",
+    "Price":12000,
+    "Name":"Kutta House"
+    }]
   constructor() { }
 
   ngOnInit() {
